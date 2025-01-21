058f7b8968962d59a754424e71501b230a5f07a5
Mahira0237 <mahira0237@gmail.com>
2025-01-21 16:43:01 +0600

    Chapter 2

---
+++ Chapter2/Chapter2.py
@@ -0,0 +1,45 @@
+#Boolean Values
+
+spam=True
+print(spam)
+
+#true True/False
+
+#True=2+2 Cant be assigned
+
+#Comparison Operators
+print(42==42)
+print(42==99)
+print(2!=4)
+print(2!=2)
+
+print('hello'=='hello')
+print('hello'=='Hello')
+print('dog'!='cat')
+print(True == True)
+print(True!=False)
+print(42==42.0)
+print(42=='42')
+
+print(42<100)
+print(42>100)
+print(42<42)
+
+eggCount = 42
+print(eggCount <= 42)
+myage=25
+print(myage>=10)
+
+print(True and True)
+print(True and False)
+
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not not not not True)
+
+print((4<5) and (5<6))
+print((4<5) and (9<6))
+print((1==2) or (2==2))
+
