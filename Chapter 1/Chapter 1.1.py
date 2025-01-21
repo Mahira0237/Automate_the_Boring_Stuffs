21edd5e889c55f40f4c98c19fde4da3f85997dd3
Mahira0237 <mahira0237@gmail.com>
2025-01-21 12:40:22 +0600

    committing Chapter 1

---
+++ Chapter 1/Chapter 1.1.py
@@ -0,0 +1,41 @@
+print(2+2)
+print((2+3)*6)
+print(2**8)
+print(23/7)
+print(23//7)
+print(23%7)
+print((5-1)*(7+1)/(3-1))
+
+#Invalid syntax
+#5+
+#42+5*2
+
+#EOL
+#'Hello
+
+#String
+print('Alice' + 'Bob')
+
+# can only concatenate string
+# print('Alice'+ 42)
+
+print('Alice'*5)
+
+#cant multiply non-int 
+#print('Alice'*'Bob')
+#ccant multiply nonint float
+#print('Alice'*5.0)
+
+#Assignment
+spam=40
+print(spam)
+eggs=2
+print(spam + eggs)
+print(spam+eggs+spam)
+spam=spam+2
+print(spam)
+
+spam='Hello'
+print(spam)
+spam='Goodbye'
+print(spam)
