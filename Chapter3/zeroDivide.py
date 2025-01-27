2fa6127fa4d1078dca3f1bc48ef5ac4545e6e049
Mahira0237 <mahira0237@gmail.com>
2025-01-27 13:25:38 +0600

    commiting Chapter 4

--- Chapter3/zeroDivide.py
+++ Chapter3/zeroDivide.py
@@ -10,6 +10,7 @@ def spam(divideBy):
     try:
         return 42 / divideBy
     except ZeroDivisionError:
+        #print('Error: Invalid argument.')
         return 'Error: Invalid argument.'
 
 print(spam(2))
